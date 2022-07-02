0c8743bb48b0d72d2a35d3abfaedb5cc32d2883e
Eduardo Fernandes Henriques <eduardofhenty@gmail.com>
2022-07-02 15:23:39 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -58,7 +58,9 @@ $(function(){
    
     
     //$("#atualiza").load("https://www.ig.com.br/ .iGh__containerDolar")   ;  
-	 $("#atualiza").load("https://www.uol.com.br/ .exchangeBarHeader__container")   ;  
+	 //$("#atualiza").load("https://www.uol.com.br/ .exchangeBarHeader__container")   ;  
+	$("#atualiza").load("https://www.uol.com.br/ .exchangeBarHeader__item__value--pos")   ;  
+	
           
         
    
